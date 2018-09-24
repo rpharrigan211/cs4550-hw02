2df98c969a164e54a423e39ec5db02a803cdd0d0
rpharrigan211 <harrigan.r@husky.neu.edu>
2018-09-24 11:20:40 -0400

    Commit 2

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -1,10 +1,23 @@
 defmodule Practice.Factor do
   def factor(x) do
-    expr
-    |> String.split(~r/\s+/)
-    |> hd
-    |> parse_float
-
-    #TODO: FIX THIS! 
+    findfactors(x, 2, [])
   end
+
+  def factor(1) do
+    []
+  end
+
+  defp findfactors(x, i, factor_list) do
+    if rem(x, i) == 0 do
+      #this adds the factor to the head of the list while performing findfactors on the number derived from dividing x by the current factor (div(x,i))
+      [i|findfactors(div(x, i), i, factor_list)]
+    else
+      if x > i do
+        findfactors(x, i + 1, factor_list)
+      else
+        factor_list
+      end
+    end
+  end
+    
 end
